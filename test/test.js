4fdad0a57fd6ed3e3dcfea8d46446b2e4e69aefb
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-01-23 17:09:25 +0530

    first commit

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const Decentragram = artifacts.require('./Decentragram.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('Decentragram', ([deployer, author, tipper]) => {
+  let decentragram
+
+  before(async () => {
+    decentragram = await Decentragram.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await decentragram.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async () => {
+      const name = await decentragram.name()
+      assert.equal(name, 'Decentragram')
+    })
+  })
+})
